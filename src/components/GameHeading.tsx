dfb24938d21276a0a87daaaef23e41922b545256
alexh8280 <99271852+alexh8280@users.noreply.github.com>
2024-05-13 23:15:13 -0400

    Fixed import in GameHeading

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,6 +1,6 @@
 import { Heading } from "@chakra-ui/react";
 import React from "react";
-import { GameQuery } from "../App";
+import { GameQuery } from "./MainPage";
 
 interface Props {
   gameQuery: GameQuery;
